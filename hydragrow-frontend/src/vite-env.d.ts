b3908d00f123fb9f610277c9e1315b22709e9623
nplongx <nplong.dev@gmail.com>
2026-08-16 14:47:35 +0700

    fix: resolve ts7016 mjs declarations and missing components

--- hydragrow-frontend/src/vite-env.d.ts
+++ hydragrow-frontend/src/vite-env.d.ts
@@ -1,10 +1,3 @@
 /// <reference types="vite/client" />
-declare module '*gleam_core/*.mjs' {
-  const content: any;
-  export = content;
-}
 
-declare module '*gleam_core/settings/*.mjs' {
-  const content: any;
-  export = content;
-}
+declare module '*.mjs';

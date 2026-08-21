8790d27005861ae1ccd53f268d2fe85a68f3831e
nplongx <nplong.dev@gmail.com>
2026-08-21 21:05:20 +0700

    .

--- hydragrow-frontend/src/firebase-messaging-sw.js
+++ hydragrow-frontend/src/firebase-messaging-sw.js
@@ -4,13 +4,13 @@ importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js
 importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js');
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyAjxXN5YIUztbY_pSpor1xsleEvHNuZqnc",
+  authDomain: "hydragrow-iot.firebaseapp.com",
+  projectId: "hydragrow-iot",
+  storageBucket: "hydragrow-iot.firebasestorage.app",
+  messagingSenderId: "810716913891",
+  appId: "1:810716913891:web:a2fea867c0d63df1bfa5d6",
+  measurementId: "G-14M8B93S7V",
 };
 
 firebase.initializeApp(firebaseConfig);

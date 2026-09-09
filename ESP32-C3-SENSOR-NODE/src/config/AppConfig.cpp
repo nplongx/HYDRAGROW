ba06dc925e3dc5d3d06ca4c2e1152365b6a4ad2a
Long Nguyễn <nplong.dev@gmail.com>
2026-09-09 21:31:42 +0700

    fix: initialize pH calibration mode in AppConfig

--- ESP32-C3-SENSOR-NODE/src/config/AppConfig.cpp
+++ ESP32-C3-SENSOR-NODE/src/config/AppConfig.cpp
@@ -2,19 +2,20 @@
 
 AppConfig appConfig = {
     {
-        2650.0f, // phV686
-        3555.0f, // phV4
-        1750.0f, // phV918
-        0.88f,   // tdsFactor
-        0.0f,    // tdsOffset
-        0.0f,    // tempOffset
-        100.0f,  // tankHeight
-        true,    // enablePh
-        true,    // enableTds
-        true,    // enableTemp
-        true     // enableWater
+        2650.0f,       // phV686
+        3555.0f,       // phV4
+        1750.0f,       // phV918
+        "2-point",     // phCalibrationMode
+        0.88f,          // tdsFactor
+        0.0f,           // tdsOffset
+        0.0f,           // tempOffset
+        100.0f,         // tankHeight
+        true,           // enablePh
+        true,           // enableTds
+        true,           // enableTemp
+        true            // enableWater
     },
-    5000,        // publishInterval
-    true,       // debugLog
-    false        // continuousLevel
+    5000,            // publishInterval
+    true,            // debugLog
+    false            // continuousLevel
 };
